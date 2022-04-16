249c628b5b08587e0409e01a450f9664fde9698b
suzukihiroki886788 <internyuuki@gmail.com>
2022-04-16 20:20:29 +0900

    commit

--- src/Create_dataset/main.py
+++ src/Create_dataset/main.py
@@ -2,20 +2,22 @@ import pandas as pd
 import numpy as np
 
 from load_dataset import Load_dataset
-from processing_dataset import Processing
-from sklearn.model_selection import train_test_split
+from processing_dataset import Processing, Split, Valid
+from Model.lightgbm import LightGBM
 
 train, test = Load_dataset()
+
 # 加工・処理
 train_df = Processing(train)
 test_df = Processing(test)
 
-X = train_df.drop(columns="id")
-target = train_df["pm25_mid"]
+# val, train, test
+X_train, X_test, y_train, y_test, index = Split(train_df)
+X_train, X_valid, y_train, y_valid = Valid(X_train, y_train)
 
-X_train, X_test, y_train, y_test = train_test_split(X,target,test_size=0.2, random_state=123)
-
-# index_df, X_train_df, X_test_df, y_train_df, y_test_df = Split(train_df)
 # 学習
+Light_GBM = LightGBM(X_train, X_valid, y_train, y_valid, fig=1)
+predict_light_gbm = Light_GBM.predict(y_train)
+print(predict_light_gbm)
 
 
--- src/Create_dataset/processing_dataset.py
+++ src/Create_dataset/processing_dataset.py
@@ -1,17 +1,6 @@
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 
-# ['id', 'year', 'month', 'day', 'Country', 'City', 'lat', 'lon', 'co_cnt',
-#        'co_min', 'co_mid', 'co_max', 'co_var', 'o3_cnt', 'o3_min', 'o3_mid',
-#        'o3_max', 'o3_var', 'so2_cnt', 'so2_min', 'so2_mid', 'so2_max',
-#        'so2_var', 'no2_cnt', 'no2_min', 'no2_mid', 'no2_max', 'no2_var',
-#        'temperature_cnt', 'temperature_min', 'temperature_mid',
-#        'temperature_max', 'temperature_var', 'humidity_cnt', 'humidity_min',
-#        'humidity_mid', 'humidity_max', 'humidity_var', 'pressure_cnt',
-#        'pressure_min', 'pressure_mid', 'pressure_max', 'pressure_var',
-#        'ws_cnt', 'ws_min', 'ws_mid', 'ws_max', 'ws_var', 'dew_cnt', 'dew_min',
-#        'dew_mid', 'dew_max', 'dew_var', 'pm25_mid']
-
 # Processing
 def Processing(df):
     drop_cols = ['year', 'month', 'day', 'Country', 'City', 'lat', 'lon']
@@ -19,9 +8,15 @@ def Processing(df):
     return df
 
 # Split
-# def Split(df):
-#     col = ["id"]
-#     X = df.drop(columns=col)
-#     target = df["pm25_mid"]
-#     X_train, X_test, y_train, y_test = train_test_split(X,target,test_size=0.2, random_state=123)
-#     return id, X_train, X_test, y_train, y_test
+def Split(df):
+    col = ["id"]
+    index = df[col]
+    X = df.drop(columns=col)
+    target = df["pm25_mid"]
+    X_train, X_test, y_train, y_test = train_test_split(X,target,test_size=0.2, random_state=123)
+    return X_train, X_test, y_train, y_test, index
+
+def Valid(X, y):
+    X_train, X_valid, y_train, y_valid = train_test_split(X,y,test_size=0.2, random_state=123)
+    return X_train, X_valid, y_train, y_valid
+
