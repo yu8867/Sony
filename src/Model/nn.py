646dca787c59bd6387a122d28fb6d72c6fd03d89
yu8867 <yu8867ki0217@gmail.com>
2022-04-17 18:16:52 +0900

    commit

---
+++ src/Model/nn.py
@@ -0,0 +1,44 @@
+from tabnanny import verbose
+import pandas as pd
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+import matplotlib.pyplot as plt
+
+def NN(X_train, X_test, y_train, y_test, fig):
+    nn1 = 64
+    nn2 = 32
+    input_dim = len(X_train.columns)
+    
+    model = Sequential()
+    model.add(Dense(nn1, activation='relu', input_dim=input_dim))
+    model.add(Dense(nn2, activation='relu'))
+    model.add(Dense(1, activation='linear'))
+
+    model.compile(optimizer='adam',
+                loss='mean_squared_error',
+                metrics=None,
+                )
+
+    train_history  = model.fit(X_train, y_train,
+            batch_size=50,
+            epochs=1000,
+            verbose=1  # type: ignore
+            )
+    
+    if fig==1:
+        plt.plot(train_history.history['loss'])
+        plt.xlabel('epochs')
+        plt.ylabel('RMSE')
+        plt.show()
+        
+    return model
+    
+    
+    
+    
+    
+    
+    
+    
+
